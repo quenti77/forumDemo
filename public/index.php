7b09f67efe932ff5af38e868189ac18d1baa7da2
quenti77 <quentin.ysambert@gmail.com>
2017-03-04 19:25:54 +0100

    Update url ask
    Change GET for SERVER field

--- public/index.php
+++ public/index.php
@@ -7,7 +7,10 @@ define('ROOT', realpath(__DIR__.'/../'));
 define('APP', realpath(ROOT.'/app'));
 
 // On récupère notre url
-$url = (isset($_GET['url'])) ? $_GET['url'] : '';
+$url = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
+if (empty($url)) {
+    $url = (isset($_GET['url'])) ? $_GET['url'] : '';
+}
 
 // On inclus les fonctions de base pour le MVC
 require APP.'/routes/router.php';
